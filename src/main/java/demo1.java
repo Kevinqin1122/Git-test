e4e1a82930d5e6abd6c4deb12f7b38b16557b14b
Kevin <Kevin@163.com>
2022-05-23 20:08:24 +0800

    third commit

--- src/main/java/demo1.java
+++ src/main/java/demo1.java
@@ -2,5 +2,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("hello java");
         System.out.println("hello php");
+        System.out.println("hello python");
     }
 }
